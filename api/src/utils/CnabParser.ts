adbb66ff47a567fd5eddb500c4c4b9c1792e328c
kallel <kallel.front@gmail.com>
2025-02-20 18:43:33 -0300

    Refactor CnabParser to improve transaction type validation and retrieve additional transaction details

--- api/src/utils/CnabParser.ts
+++ api/src/utils/CnabParser.ts
@@ -1,30 +1,28 @@
-import { transactionTypes } from "./TransactionTypes";
+import { getTransactionType } from "./TransactionTypes";
 import { ITransactionInput } from "../models/Transaction";
 
 export default class CnabParser {
   static parse(line: string): ITransactionInput {
-    const type = parseInt(line.substring(0, 1), 10);
-    if (!transactionTypes[type]) {
-      throw new Error(`Invalid transaction type: ${type}`);
+    const typeStr = line.substring(0, 1);
+    const type = parseInt(typeStr, 10);
+
+    if (isNaN(type)) {
+      throw new Error(`Invalid transaction type: ${typeStr}`);
     }
+
+    const { description, nature, signal } = getTransactionType(type);
 
     const date = new Date(
       parseInt(line.substring(1, 5)),
       parseInt(line.substring(5, 7)) - 1,
       parseInt(line.substring(7, 9))
     );
-
     const amount = parseFloat(line.substring(9, 19)) / 100;
     const cpf = line.substring(19, 30);
     const card = line.substring(30, 42);
     const time = line.substring(42, 48);
     const storeOwner = line.substring(48, 62).trim();
     const storeName = line.substring(62, 81).trim();
-
-    const { nature, signal } = transactionTypes[type] as {
-      nature: "Entrada" | "Saída";
-      signal: "+" | "-";
-    };
 
     return {
       type,
@@ -35,6 +33,7 @@ export default class CnabParser {
       time,
       storeOwner,
       storeName,
+      description,
       nature,
       signal,
     };

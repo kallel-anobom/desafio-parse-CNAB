a6aee992b5f9dba08b29b6c0d94f5a763941c7ec
kallel <kallel.front@gmail.com>
2025-02-22 00:51:36 -0300

    Add formatCurrency utility function for currency formatting

---
+++ app/src/utils/formatCurrency.ts
@@ -0,0 +1,10 @@
+export const formatCurrency = (
+  value: number,
+  locale: string = "pt-BR",
+  currency: string = "BRL"
+): string => {
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency,
+  }).format(value);
+};
